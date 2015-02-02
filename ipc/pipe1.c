af11dfa9add3cbb9798a589054e9fa10d6316b84
liangch <liangch@vendor.marvell.com>
2015-02-02 15:51:11 +0800

    test pipe-1

--- ipc/pipe1.c
+++ ipc/pipe1.c
@@ -1,4 +1,9 @@
-#include "apue.h"
+//#include "apue.h"
+#include <stdio.h>      /* for convenience */
+#include <stdlib.h>     /* for convenience */
+#include <unistd.h>     /* for convenience */
+
+#define MAXLINE 4096            /* max line length */
 
 int main(void)
 {
@@ -8,10 +13,12 @@ int main(void)
 	char	line[MAXLINE];
 
 	if (pipe(fd)<0) {
-		err_sys("pipe error");
+		//err_sys("pipe error");
+		printf("pipe error");
 	}
 	if ((pid = fork()) < 0) {
-		err_sys("fork error");
+		//err_sys("fork error");
+		printf("fork error");
 	} else if (pid > 0) {	/* parent */
 		close(fd[0]);
 		write(fd[1], "hello world\n", 12);
